9a3386af9178c4916aabb47bd9fbd69202bc82f9
unknown <johndoe@example.com>
2020-10-24 10:11:50 +0800

    basic function completed

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models heddre.
-from .models import Post
+from .models import Post,Comment
 
 admin.site.register(Post)
+admin.site.register(Comment)
