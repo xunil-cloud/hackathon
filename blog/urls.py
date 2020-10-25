46557fccd93296f2692a7da737c0d7e41a2f7c36
benson <example@example.com>
2020-10-25 10:20:17 +0800

    add image for post, remove db from git

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
-
+from django.views.static import serve
+from django.urls import path, include, re_path
+from mysite.settings import MEDIA_ROOT
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('onecolumn', views.onecolumn, name='onecolumn'),
@@ -8,4 +10,5 @@ urlpatterns = [
     #path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
     path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
     path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+    re_path('media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
 ]
